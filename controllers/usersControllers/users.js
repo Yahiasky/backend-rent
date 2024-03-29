9e83c4be356ac911b9d503213d6ab2e10dfafc88
Yahiasky <129469342+Yahiasky@users.noreply.github.com>
2024-03-29 02:14:27 +0000

    fix

--- controllers/usersControllers/users.js
+++ controllers/usersControllers/users.js
@@ -16,10 +16,25 @@ var getUsers=async (req,res)=>{
 
 var getUser=async(req,res)=>{
 
-  
+   
+    const rateAVG_asClient=await connection_MySQL.query(`SELECT AVG(value) FROM rateClient,rent 
+    where rent.idUser ='${req.params.idUser}' and rateClient.idRent=rent.idRent ;`)
+   const rateAVG_asOwner=await connection_MySQL.query(`SELECT U.idUser, AVG(temp.rateAVG) AS UserRate
+   FROM User U 
+   JOIN (
+       SELECT A.idUser, AVG(RV.value) AS rateAVG
+       FROM apartment A 
+       JOIN rent R ON A.idApartment = R.idApartment
+       JOIN Review RV ON R.idRent = RV.idRent
+       GROUP BY A.idUser
+   ) AS temp ON U.idUser = temp.idUser
+   GROUP BY U.idUser;
+   
+   `)
+   console.log(rateAVG_asOwner)
    const data=await connection_MySQL.query(`SELECT * FROM User where idUser ='${req.params.idUser}' ;`)
    var [FinalData]=data[0]
-  return FinalData==null ? res.sendStatus(204) :res.json(FinalData)
+  return FinalData==null ? res.sendStatus(204) :res.json({userData:FinalData,userRateAsClient:rateAVG_asClient[0]})
    
 
 }
