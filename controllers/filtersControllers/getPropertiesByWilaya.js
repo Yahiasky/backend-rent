160454cddff30af11ef7d3b9d4a5744e8fdb41b0
Yahiasky <129469342+Yahiasky@users.noreply.github.com>
2024-03-29 01:11:24 +0000

    fix filters api

--- controllers/filtersControllers/getPropertiesByWilaya.js
+++ controllers/filtersControllers/getPropertiesByWilaya.js
@@ -2,7 +2,8 @@ const connection_MySQL=require('../../MySql/connect')
 
 
 var getPropsByWilaya=async(req,res)=>{
-    const data=await connection_MySQL.query(`SELECT * FROM apartment where wilaya =${req.params.wilaya} ;`)
+    const data=await connection_MySQL.query(`SELECT * FROM apartment,User where wilaya =${req.params.wilaya} 
+    and apartment.idUser=User.idUser ;`)
     var [FinalData]=data
    return FinalData==null ? res.sendStatus(204) :res.json(FinalData)
   
